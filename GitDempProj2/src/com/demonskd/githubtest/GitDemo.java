471b16669f28c5ce32447f1f34597f4dc95febf0
skdhitman <piyushojha.skd@gmail.com>
2021-07-16 18:58:34 +0530

    First commit of GitDemo.java to local and remote at once

---
+++ GitDempProj2/src/com/demonskd/githubtest/GitDemo.java
@@ -0,0 +1,12 @@
+package com.demonskd.githubtest;
+
+public class GitDemo {
+	public static void main (String[] args) {
+		String code = "skd";
+		if (code == "skd") {
+			System.out.println("You are the best!");
+		}
+		else
+			System.out.println("Not the best!");
+	}
+}
